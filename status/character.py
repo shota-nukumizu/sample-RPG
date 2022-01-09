0c04054fe690d517bbb6ead84c7ed685672822be
shota-nukumizu <author2000.1225@gmail.com>
2022-01-09 10:01:22 +0900

    create status display

--- status/character.py
+++ status/character.py
@@ -1,6 +1,6 @@
 class Unit:
 
-    def __init__(self, name, hp, mp, attack, defence, speed, resistance, skill, lv):
+    def __init__(self, name, hp, mp, attack, defence, speed, resistance, skill, lv, job):
         self.name = name
         self.hp = hp
         self.mp = mp
@@ -10,7 +10,22 @@ class Unit:
         self.resistance = resistance
         self.skill = skill
         self.lv = lv
+        self.job = job
     
     def __str__(self):
         return self.name
     
+    def show_status(self):
+        return f'{self.name} | HP: {self.hp}, MP: {self.mp}, LEVEL: {self.lv}, JOB: {self.job}'
+    
+    def show_more_status(self):
+        return f'''Name: {self.name}
+        HP: {self.hp}
+        MP: {self.mp}
+        LEVEL: {self.lv}
+        JOB: {self.job}
+        ATK: {self.attack}
+        DEF: {self.defence}
+        RES: {self.resistance}
+        SKILL: {self.skill}
+        '''
